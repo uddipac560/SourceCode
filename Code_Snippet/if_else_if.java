5ebaf88e7397993096105781f824afb90af58f9f
ganievz218 <ganievz218@niagaracatholic.ca>
2025-01-07 09:02:15 -0500

    Create if_else_if.java

---
+++ Code_Snippet/if_else_if.java
@@ -0,0 +1,12 @@
+if (drinkSize.equals("Large")) 
+{
+    total += 1.39;
+} 
+else if (drinkSize.equals("Medium")) 
+{
+    total += 1.19;
+} 
+else { // Assumes drink size is "Small"
+{
+    total += 0.99;
+}
