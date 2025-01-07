3710b88b6039c140b52ee75213042e150d303a39
ganievz218 <ganievz218@niagaracatholic.ca>
2025-01-07 09:04:49 -0500

    Create mouse_events.java

---
+++ Code_Snippet/mouse_events.java
@@ -0,0 +1,8 @@
+Import the java.awt.event.* library
+Implement the interface of the MouseListener class
+For MouseListener the programmer must implement the methods:
+mouseEntered()
+mouseExited()
+mousePressed()
+mouseReleased()
+mouseClicked()
