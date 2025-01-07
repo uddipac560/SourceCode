abccc4bd037d2ce8e389073038f8ceb44a6cf760
ganievz218 <ganievz218@niagaracatholic.ca>
2025-01-07 08:05:05 -0500

    Create Header.java

---
+++ Code_Snippet/Header.java
@@ -0,0 +1,7 @@
+/* ..... */
+
+Line 1 - Name of the Program
+Line 2 - Version of the Program (Version starts with 0.1 until approved.)
+Line 3 - Author of the Program
+Line 4 - Target of the Program (i.e. your teacher)
+Line 5 - Purpose
