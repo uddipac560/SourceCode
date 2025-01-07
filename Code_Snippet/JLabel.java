6f7eb5573036d93f91c2adaeef77b3da44e1e6f5
ganievz218 <ganievz218@niagaracatholic.ca>
2025-01-07 08:57:23 -0500

    Create JLabel.java

---
+++ Code_Snippet/JLabel.java
@@ -0,0 +1,3 @@
+JLabel lab = new JLabel("Enter name: "); // Create a JLabel with initial text
+lab.setText("New Message"); // Update the text of the JLabel
+String s = lab.getText(); // Retrieve the current text of the JLabel (rarely used)
